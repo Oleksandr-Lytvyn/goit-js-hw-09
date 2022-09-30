83ce4b88faa39a4098bad283846de1f92ae93593
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-09-30 21:26:44 +0300

    add promise and timeout

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+const formRef = document.querySelector('form');
+let firstDelay = document.querySelector('[name="delay"]');
+let step = document.querySelector('[name="step"]');
+let amount = document.querySelector('[name="amount"]');
+console.log(firstDelay);
+console.log(step);
+console.log(amount);
+
+formRef.addEventListener('submit', onSubmit)
+
+function onSubmit(event) {
+  event.preventDefault()
+  createPromise()
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const timeout = firstDelay.value * 1000;
+  console.log(timeout);
+  return new Promise((resolve, reject) => {
+    // Asynchronous operation
+    setTimeout(()=>{
+      if (shouldResolve) {
+        // Fulfill
+        ;
+        resolve(position,delay,console.log('success'))
+        ;
+      } else {
+        // Reject
+        reject(position, delay)
+      }
+    },timeout)
+    
+  });
+  
 }
+
+
+
